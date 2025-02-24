80154557e3a0a19832952fd8aeee6b7e16c6e71f
zayan <oddlyflavored@gmail.com>
2025-02-24 01:25:31 +0000

    og metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,32 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Science Bowl Practice',
-  description: 'Practice your Science Bowl questions',
+  title: "Sci-Buddy",
+  description: "Audio-based questions for a realistic Science Bowl practice experience.",
+  openGraph: {
+    title: "Sci-Buddy",
+    description: "Audio-based questions for a realistic Science Bowl practice experience.",
+    url: "https://www.sci-buddy.com/", // Replace with your actual domain
+    type: "website",
+    images: [
+      {
+        url: "/public/sci-buddy.png", 
+        width: 1200,
+        height: 630,
+        alt: "Sci-Buddy Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sci-Buddy",
+    description: "Audio-based questions for a realistic Science Bowl practice experience.",
+    images: ["/public/sci-buddy.png"], 
+  },
+  robots: "index, follow", 
+  viewport: "width=device-width, initial-scale=1", 
 };
+
 
 export default function RootLayout({
   children,
